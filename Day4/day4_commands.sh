811cfc9324640af70fcaf02d38bd205a50573793
Nada-Mostafa31 <nada.mostafa@ejust.edu.eg>
2025-09-02 23:38:36 +0300

    Day 4: Process and Network Monitoring

---
+++ Day4/day4_commands.sh
@@ -0,0 +1,14 @@
+  705  nano ~/iot_logger/scripts/sensor_poll.sh
+  706  chmod +x ~/iot_logger/scripts/sensor_poll.sh
+  707  ls ~/iot_logger/scripts
+  708  cat sensor_poll.sh
+  709  cd ~/iot_logger/scripts
+  710  cat sensor_poll.sh
+  711  ./sensor_poll.sh &
+  712  jobs
+  713  ps aux | grep sensor_poll
+  714  ss -tulwn
+  715  bg
+  716  fg
+  722  ps aux | grep sensor_poll
+  723  kill 9399
